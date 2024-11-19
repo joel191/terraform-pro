1521164acef27b2b04f14276127243390567ddba
root <root@ip-172-31-32-143.ec2.internal>
2024-11-19 14:10:13 +0000

    commiting

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,10 @@
-resource "aws_instance" "demoinstance" {
+resource "aws_instance" "public_subnet-1" {
 ami ="ami-012967cc5a8c9f891"
 instance_type="t2.micro"
 count=1
 key_name="joel"
 vpc_security_group_ids=["${aws_security_group.demosg.id}"]
-subnet_id="${aws_subnet.public_subnet-1.id}"
+subnet_id=aws_subnet.public_subnet-1.id
 associate_public_ip_address=true
 user_data="${file("data.sh")}"
 tags={
@@ -12,13 +12,13 @@ Name="My Public Instance"
 }
 }
 
-resource "aws_instance" "demoinstance1" {
+resource "aws_instance" "public_subnet-2" {
 ami="ami-012967cc5a8c9f891"
 instance_type="t2.micro"
 count=1
 key_name="joel"
 vpc_security_group_ids=["${aws_security_group.demosg.id}"]
-subnet_id="${aws_subnet.public_subnet-2.id}"
+subnet_id=aws_subnet.public_subnet-2.id
 associate_public_ip_address=true
 user_data="${file("data.sh")}"
 tags={
